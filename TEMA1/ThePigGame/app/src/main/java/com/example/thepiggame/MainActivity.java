5b2081356a55260d882e12b26543b357f9a2f565
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-03 00:53:39 +0200

    Add comentarios

--- TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/MainActivity.java
+++ TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/MainActivity.java
@@ -10,92 +10,116 @@ import android.widget.TextView;
 import com.example.thepiggame.gameFun.RollDices;
 
 public class MainActivity extends AppCompatActivity {
-    private  int roll = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Players
         final String player1 = "player1", player2 = "player2";
 
 
-
+        //Buttons
         Button restartGame = findViewById(R.id.newGameButton);
         Button rollDice = findViewById(R.id.rollDiceButton);
         Button hold = findViewById(R.id.holdButton);
 
+        //Game
         RollDices game = new RollDices();
+
         rollDice.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method is called when the roll dice button is clicked.
+             * @param view
+             */
             @Override
             public void onClick(View view) {
 
-
-                if(!game.isHold()){
+                //If the player is not hold, the player 1 is playing, else the player 2 is playing.
+                if(game.invertedHold()){
                     game.play();
 
-
+                    //Update the current value of the player 1.
                     TextView player1View = findViewById(R.id.currentValuePlayer1);
-                    player1View.setText(String.valueOf(game.getRoundScore()));
+                    player1View.setText(String.valueOf(RollDices.getRoundScore()));
                 }else{
                    game.play();
 
+                   //Update the current value of the player 2.
                     TextView player2View = findViewById(R.id.currentValuePlayer2);
-                    player2View.setText(String.valueOf(game.getRoundScore()));
+                    player2View.setText(String.valueOf(RollDices.getRoundScore()));
                 }
             }
         });
 
         restartGame.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method is called when the restart button is clicked.
+             * @param view
+             */
             @Override
             public void onClick(View view) {
                 game.deletePoints();
 
+                //Update the current value of the player 2.
                 TextView player2View = findViewById(R.id.player2Contador);
                 player2View.setText(getResources().getString(R.string.numberZero));
 
+                //Update the current value of the player 1.
                 TextView currentValuePlayer2 = findViewById(R.id.currentValuePlayer2);
                 currentValuePlayer2.setText(getResources().getString(R.string.currentValue));
 
+                //Update the current value of the player 1.
                 TextView player1View = findViewById(R.id.player1Contador);
                 player1View.setText(getResources().getString(R.string.numberZero));
 
+                //Update the current value of the player 1.
                 TextView currentValuePlayer1 = findViewById(R.id.currentValuePlayer1);
                 currentValuePlayer1.setText(getResources().getString(R.string.currentValue));
             }
         });
 
         hold.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method is called when the hold button is clicked.
+             * @param view
+             */
             @Override
             public void onClick(View view) {
 
-                if(!game.isHold()){
-                    game.sumDice(player1, game.getRoundScore());
-                    roll = 0;
-                    game.setRoundScore(0);
+                //If the player is not hold, the player 1 is playing, else the player 2 is playing.
+                if(game.invertedHold()){
 
+                    //Update the current value of the player 1.
+                    game.sumDice(player1, RollDices.getRoundScore());
+                    RollDices.setRoundScore(0);
+
+                    //Update the current value of the player 1.
                     TextView player1View = findViewById(R.id.player1Contador);
                     player1View.setText(String.valueOf(game.getPuntuacion(player1)));
 
 
                 }else{
-                    game.sumDice(player2, game.getRoundScore());
-                    roll = 0;
-                    game.setRoundScore(0);
+                    //Update the current value of the player 2.
+                    game.sumDice(player2, RollDices.getRoundScore());
+                    RollDices.setRoundScore(0);
+
+                    //Update the current value of the player 2.
                     TextView player2View = findViewById(R.id.player2Contador);
                     player2View.setText(String.valueOf(game.getPuntuacion(player2)));
-
-
                 }
 
-                game.setHold(!game.isHold());
+                //Change the player.
+                game.setHold(game.invertedHold());
 
 
-
+                //Update the current value of the player 2.
                 TextView currentValuePlayer1 = findViewById(R.id.currentValuePlayer1);
                 currentValuePlayer1.setText(getResources().getString(R.string.currentValue));
 
+                //Update the current value of the player 2.
                 TextView currentValuePlayer2 = findViewById(R.id.currentValuePlayer2);
                 currentValuePlayer2.setText(getResources().getString(R.string.currentValue));
             }
--- TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/gameFun/RollDices.java
+++ TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/gameFun/RollDices.java
@@ -2,62 +2,104 @@ package com.example.thepiggame.gameFun;
 
 import java.util.HashMap;
 
+/**
+ * This class is the game.
+ */
 public class RollDices {
-    public HashMap<String, Integer> getPlayers() {
-        return players;
-    }
 
-    private HashMap<String,Integer> players = new HashMap<>();
-    private boolean hold = false;
+    private final HashMap<String,Integer> players = new HashMap<>(); //private String player1 = "player1", player2 = "player2";
 
-    private static int roundScore = 0;
 
+    private boolean hold = false; //private boolean player1Playing = true, player2Playing = false;
+
+
+    private static int roundScore = 0;  //private int player1Score = 0, player2Score = 0;
+
+    /**
+     * This constructor is used to initialize the players.
+     */
     public RollDices(){
             players.put("player1", 0);
             players.put("player2", 0);
     }
+
+    /**
+     * This method is used to play the game.
+     * If the player has not hold, the player is playing, else the player is not playing.
+     */
     public void play(){
         int n = 0;
         if(!((n = Dice.throwDices())==1)){
             roundScore += n;
         }else{
-            this.setHold(!this.isHold());
+            this.setHold(this.invertedHold());
             roundScore=0;
         }
 
     }
-
-
-    public void sumDice(String player, int roll){
-        Integer currentScore = players.get(player);
-        if (currentScore == null) {
-            players.put(player, roll);
-        } else {
-            players.put(player, currentScore + roll);
+    /**
+     * This method is used to check if the player has won.
+     */
+    public void winner(){
+        if(players.get("player1") >= 100){
+            System.out.println("Player 1 wins");
+        }else if(players.get("player2") >= 100){
+            System.out.println("Player 2 wins");
         }
     }
 
+    /**
+     * This method is used to sum the dice to the player.
+     * @param player is the player.
+     * @param roll is the value of the dice.
+     */
+    public void sumDice(String player, int roll){
+        players.merge(player, roll, Integer::sum);
+    }
+
+    /**
+     * This method is used to delete the points of the players.
+     */
     public void deletePoints(){
        this.players.replace("player1", 0);
         this.players.replace("player2", 0);
     }
 
+    /**
+     * This method is used to set the player.
+     */
     public void setHold(Boolean hold){
         this.hold = hold;
     }
 
-    public boolean isHold(){
-        return this.hold;
+    /**
+     * This method is used to check if the player is hold.
+     */
+    public boolean invertedHold(){
+        return !this.hold;
     }
 
+    /**
+     * This method is used to set the round score.
+     * @param roundScore Current round score.
+     */
     public static void setRoundScore(int roundScore) {
         RollDices.roundScore = roundScore;
     }
 
+    /**
+     * This method is used to get the round score.
+     * @return Current round score.
+     */
     public static int getRoundScore() {
         return roundScore;
     }
 
+    /**
+     * This method is used to get the score of the player.
+     * @param player Player.
+     * @return Score of the player.
+     */
     public int getPuntuacion(String player){
         Integer score = players.get(player);
         if (score == null) {
