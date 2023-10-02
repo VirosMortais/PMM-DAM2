e42279931f7613eeaa89963d5fc925e9f75f4b4c
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-02 20:35:01 +0200

    Implementado la funcinalidad del button
    Aumente el tamaño del TextView para 29dp

--- TEMA1/ContadorDeClics/app/src/main/java/com/example/contadordeclics/MainActivity.java
+++ TEMA1/ContadorDeClics/app/src/main/java/com/example/contadordeclics/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.contadordeclics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int contador;
+    TextView contadorDeClics;
+    Button augmentarElContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contadorDeClics = findViewById(R.id.contadorDeClics);
+        augmentarElContador = findViewById(R.id.augmentarElContador);
+
+        augmentarElContador.setOnClickListener(view -> {
+            contador++;
+            contadorDeClics.setText("Has clicado " + contador + " veces");
+        });
     }
 }
