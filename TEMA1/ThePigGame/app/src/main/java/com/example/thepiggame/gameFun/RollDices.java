f054e12d8865a9c146d3a2ba9c58c012e6b36b86
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-03 00:24:30 +0200

    Implemente todos lo metodos nescesarios para que el juego funciones.
    @version 1.1

--- TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/gameFun/RollDices.java
+++ TEMA1/ThePigGame/app/src/main/java/com/example/thepiggame/gameFun/RollDices.java
@@ -1,28 +1,69 @@
 package com.example.thepiggame.gameFun;
 
+import java.util.HashMap;
+
 public class RollDices {
-
-    private Player player1;
-    private Player player2;
-
-    public RollDices(Player player1, Player player2){
-        this.player1 = player1;
-        this.player2 = player2;
+    public HashMap<String, Integer> getPlayers() {
+        return players;
     }
 
+    private HashMap<String,Integer> players = new HashMap<>();
+    private boolean hold = false;
+
+    private static int roundScore = 0;
+
+    public RollDices(){
+            players.put("player1", 0);
+            players.put("player2", 0);
+    }
     public void play(){
-        int tiro1 = Dice.throwDices();
-        int tiro2 = Dice.throwDices();
+        int n = 0;
+        if(!((n = Dice.throwDices())==1)){
+            roundScore += n;
+        }else{
+            this.setHold(!this.isHold());
+            roundScore=0;
+        }
 
-        player1.sumDice(tiro1);
-        player2.sumDice(tiro2);
     }
 
-    public Player getPlayer1() {
-        return player1;
+
+    public void sumDice(String player, int roll){
+        Integer currentScore = players.get(player);
+        if (currentScore == null) {
+            players.put(player, roll);
+        } else {
+            players.put(player, currentScore + roll);
+        }
     }
 
-    public Player getPlayer2() {
-        return player2;
+    public void deletePoints(){
+       this.players.replace("player1", 0);
+        this.players.replace("player2", 0);
+    }
+
+    public void setHold(Boolean hold){
+        this.hold = hold;
+    }
+
+    public boolean isHold(){
+        return this.hold;
+    }
+
+    public static void setRoundScore(int roundScore) {
+        RollDices.roundScore = roundScore;
+    }
+
+    public static int getRoundScore() {
+        return roundScore;
+    }
+
+    public int getPuntuacion(String player){
+        Integer score = players.get(player);
+        if (score == null) {
+            return 0;
+        } else {
+            return score;
+        }
     }
 }
