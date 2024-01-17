4ed80942b9fe88c4550cc9c7c48226884e99786b
chaarrsan <charlestacodeouro@hotmail.com>
2024-01-17 20:04:16 +0100

    Pratica terminada

---
+++ TEMA2/Comarcas/README.md
@@ -0,0 +1,71 @@
+# ComarquesVirosms
+
+![Logo](https://github.com/VirosMs/ComarquesVirosMs/assets/94723454/707f496c-3b03-4f56-9a3c-db9dbdf6a4cd)
+
+## Descripción del Proyecto
+
+Este proyecto es una aplicación de línea de comandos desarrollada en Dart para interactuar con una API de comarcas. La aplicación permite obtener información sobre las comarcas de una provincia específica y detalles adicionales sobre una comarca en particular.
+
+La aplicación consta de dos funciones principales:
+
+- `comarcas(String provincia)`: Muestra una lista de comarcas de una provincia específica.
+
+- `inforComarca(String comarca)`: Muestra detalles sobre una comarca específica, incluyendo su nombre, capital, población, imagen, descripción, latitud y longitud.
+
+## Configuración
+
+Para ejecutar este proyecto, necesitarás tener instalado Dart en tu máquina.
+
+## Dependencias
+
+Este proyecto depende de la biblioteca [http](https://pub.dev/packages/http) para realizar solicitudes HTTP. Esta dependencia está definida en el archivo `pubspec.yaml` del proyecto.
+
+## Ejecución
+
+Para ejecutar este proyecto, abre una terminal en el directorio del proyecto y ejecuta el siguiente comando:
+
+```bash
+dart bin/Main.dart
+```
+
+## Cómo funciona
+
+Cuando ejecutas la aplicación, puedes proporcionar argumentos para obtener información sobre las comarcas de una provincia o detalles sobre una comarca específica.
+
+## Estructura del Proyecto
+
+El proyecto consta de un archivo principal:
+
+- `Main.dart`: Este es el archivo principal que contiene las funciones para interactuar con la API de comarcas.
+
+
+## Licencia
+
+Este proyecto está licenciado bajo los términos de la licencia MIT. Para más detalles, consulta el archivo [MIT license](https://github.com/VirosMs/ComarquesVirosMs?tab=MIT-1-ov-file) en el directorio raíz del proyecto.
+
+## Instalación
+
+Para instalar este proyecto, necesitarás tener Dart instalado en tu sistema. Aquí están los pasos para instalar y ejecutar el proyecto:
+
+1. Clona el repositorio en tu máquina local usando Git:
+
+```bash
+git clone https://github.com/VirosMortais/comarquesvirosms.git
+cd comarquesvirosms
+```
+
+2. Ejecuta el proyecto con el siguiente comando:
+
+```bash
+dart bin/Main.dart
+```
+
+## Acknowledgements
+
+- [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+- [Awesome README](https://github.com/matiassingers/awesome-readme)
+- [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+## Authors
+
+- [@VirosMS](https://github.com/VirosMs)
--- TEMA2/Comarcas/bin/Main.dart
+++ TEMA2/Comarcas/bin/Main.dart
@@ -1,45 +1,136 @@
-import 'dart:io';
-
-import 'package:http/http.dart' as http;
+// Importing necessary libraries
 import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
-String uri = "https://node-comarques-rest-server-production.up.railway.app/api/comarques/";
+// Base URI for the API
+String uri = 'https://node-comarques-rest-server-production.up.railway.app/api/comarques/';
 
-void main(List<String> args){
-
-  if(args.isEmpty){
+// Main function
+void main(List<String> args) {
+  // Check if arguments are provided
+  if (args.isEmpty) {
     print("No se ha introducido ningún argumento");
     exit(1);
   }
 
-  if(args[0] == "comarcas"){
+  // Check the first argument and call the appropriate function
+  if (args[0] == 'comarcas') {
     comarcas(args[1]);
-  }else if(args[0] == "infocomarca") {
-    inforComarca(args[1]);
-  }else{
+  } else if (args[0] == 'infocomarca') {
+    var comarca = "";
+
+    // Concatenate all arguments after the first one
+    for (var element in args.skip(1)) {
+      if(element !=args.last){
+        comarca += element + ' ';
+      }else{
+        comarca += element;
+      }
+    }
+    inforComarca(comarca);
+  } else {
     print("No se ha introducido ningún argumento válido");
     exit(1);
   }
-
-
 }
 
-void comarcas(String provincia){
-  var response = http.get(Uri.parse(uri));
-  var bodyJson;
-  response.then((value) {
-    if(value.statusCode == 200){
-      String body = utf8.decode(value.bodyBytes);
-      bodyJson = jsonDecode(body) as List;
+// Function to get all comarcas of a province
+comarcas(String provincia) async {
+  print("Comarcas de la provincia de " + provincia + "\n");
+
+  try {
+    // Send a GET request to the API
+    var response = await http.get(Uri.parse(uri + provincia));
+
+    // Check the status code of the response
+    if (response.statusCode == 200) {
+      var body = utf8.decode(response.bodyBytes);
+
+      // Parse the response body and print each comarca
+      List<String> listComarcas = List.from(jsonDecode(body));
+      listComarcas.forEach((element) => print(element));
+    } else {
+      print(response.statusCode);
+      exit(1);
     }
+  } catch (e) {
+    throw Exception("Error al conectar con el servidor");
+  }
+}
+
+// Function to get information about a comarca
+void inforComarca(String comarca) async {
+  print("Información de la comarca de " + comarca + "\n");
+
+  try {
+    // Send a GET request to the API
+    var response = await http.get(Uri.parse(uri + 'infocomarca/' + comarca));
+
+    // Check the status code of the response
+    if (response.statusCode == 200) {
+      Map<String, dynamic> comarca;
+
+      try {
+        // Parse the response body
+        comarca = jsonDecode(response.body);
+      } catch (e) {
+        throw Exception("Error al decodificar el JSON");
+      }
+
+      // Create a Comarca object from the parsed data
+      Comarca comarcaObj = Comarca(
+        nombre: comarca['comarca'],
+        capital: comarca['capital'],
+        poblacion: comarca['poblacio'],
+        img: comarca['img'],
+        desc: comarca['desc'],
+        latitud: comarca['latitud'],
+        longitud: comarca['longitud'],
+      );
+
+      // Print the Comarca object
+      print(comarcaObj);
+    } else {
+      print(response.statusCode);
+      exit(1);
+    }
+
+  } catch (e) {
+    throw Exception("Error al conectar con el servidor");
+  }
+}
+
+// Class to represent a Comarca
+class Comarca {
+  String nombre;
+  String? capital;
+  String? poblacion;
+  String? img;
+  String? desc;
+  double? latitud;
+  double? longitud;
+
+  // Constructor for the Comarca class
+  Comarca({
+    required this.nombre,
+    required this.capital,
+    required this.poblacion,
+    required this.img,
+    required this.desc,
+    required this.latitud,
+    required this.longitud,
   });
 
-  for(var comarca in bodyJson){
-    print(comarca);
+  // Override the toString method to print a Comarca object in a specific format
+  @override
+  String toString() {
+    return "Comarca: " + nombre + "\n" +
+    "Capital: " + capital! + "\n" +
+    "Población: " + poblacion! + "\n" +
+    "Imagen: " + img! + "\n" +
+    "Descripción: " + desc! + "\n" +
+    "Latitud: " + latitud.toString() + "\n" +
+    "Longitud: " + longitud.toString() + "\n";
   }
-
-}
-
-void inforComarca(String comarca){
-
 }
